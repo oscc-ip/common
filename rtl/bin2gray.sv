aa2e57927c57ebecbd04f0a0fb8e3c24f1b02f2c
maksyuki <maksyuki@126.com>
2023-10-06 15:08:36 +0800

    feat: add bin2gray and gray2bin module

---
+++ rtl/bin2gray.sv
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// common is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+module bin2gray #(
+    parameter int DATA_WIDTH = 1
+) (
+    input logic [DATA_WIDTH-1:0] bin_i,
+    input logic [DATA_WIDTH-1:0] gray_o
+);
+
+  assign gray_o = bin_i ^ (bin_i >> 1);
+endmodule
---
+++ rtl/gray2bin.sv
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// common is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+module gray2bin #(
+    parameter int DATA_WIDTH = 1
+) (
+    input logic [DATA_WIDTH-1:0] gray_i,
+    input logic [DATA_WIDTH-1:0] bin_o
+);
+
+  assign bin_o[DATA_WIDTH-1] = gray_i[DATA_WIDTH-1];
+  genvar i;
+  generate
+    for (i = 0; i < DATA_WIDTH - 1; i++) begin
+      assign bin_o[i] = bin_o[i+1] ^ gray_i[i];
+    end
+  endgenerate
+endmodule
