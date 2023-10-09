a336d28a48d16405a131f31a205cd3b13dbaec73
maksyuki <maksyuki@126.com>
2023-10-09 20:30:12 +0800

    feat: modify clk int div module

--- rtl/clkrst/clk_int_div.sv
+++ rtl/clkrst/clk_int_div.sv
@@ -8,17 +8,25 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-module clk_int_div #(
+module clk_int_even_div #(
+    parameter int DIV_VALUE = 2
+) (
+    input  logic clk_i,
+    input  logic rst_n_i,
+    input  logic en_i,
+    output logic clk_o
+);
+
+endmodule
+
+
+module clk_int_odd_div #(
     parameter int DIV_VALUE = 1
 ) (
     input  logic clk_i,
     input  logic rst_n_i,
     input  logic en_i,
-    input  logic test_en_i,
     output logic clk_o
 );
-
-  localparam logic IsOddDiv = DIV_VALUE[0];
-  localparam logic IsBypass = (DIV_VALUE < 2) ? 1'b1 : 1'b0;
 
 endmodule
