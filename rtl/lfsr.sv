7ca27d345b42f90d5f5e580e98d8b8243d39b0aa
maksyuki <maksyuki@126.com>
2023-10-22 14:56:25 +0800

    feat: modify lfsr module

--- rtl/lfsr.sv
+++ rtl/lfsr.sv
@@ -8,32 +8,64 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
+//https://en.wikipedia.org/wiki/Linear-feedback_shift_register
 module lfsr_galois #(
-    parameter int          DATA_WIDTH = 32,
-    parameter int unsigned SEED       = 0
+    parameter int                    DATA_WIDTH = 32,
+    parameter logic [DATA_WIDTH-1:0] POLY       = '0
 ) (
     input  logic                  clk_i,
     input  logic                  rst_n_i,
+    input  logic                  wr_i,
+    input  logic [DATA_WIDTH-1:0] dat_i,
     output logic [DATA_WIDTH-1:0] dat_o
 );
 
   logic [DATA_WIDTH-1:0] s_shift_d, s_shift_q;
-
-  assign s_shift_d[0]              = s_shift_q[1];
-  assign s_shift_d[DATA_WIDTH-1:0] = s_shift_q[0];
-  for (genvar i = 1; i < DATA_WIDTH - 1; i++) begin
-    assign s_shift_d[i] = s_shift_q[i+1] ^ s_shift_q[i-1];
+  for (genvar i = 0; i < DATA_WIDTH - 1; i++) begin
+    if (POLY & (1 << i)) begin
+      assign s_shift_d[i] = wr_i ? dat_i[i] : s_shift_q[i+1] ^ s_shift_q[0];
+    end else begin
+      assign s_shift_d[i] = wr_i ? dat_i[i] : s_shift_q[i+1];
+    end
   end
 
-  dffrc #(32, SEED) u_shift_dffrc (
+  assign dat_o = s_shift_q;
+  dffr #(DATA_WIDTH) u_shift_dffrc (
       clk_i,
       rst_n_i,
       s_shift_d,
       s_shift_q
   );
 
-
 endmodule
 
-module lfsr_fibonacci ();
+// due to the delay, more recommand to use galois type lfsr
+module lfsr_fibonacci #(
+    parameter int                    DATA_WIDTH = 32
+) (
+    input  logic                  clk_i,
+    input  logic                  rst_n_i,
+    input  logic                  wr_i,
+    input  logic [DATA_WIDTH-1:0] dat_i,
+    output logic [DATA_WIDTH-1:0] dat_o
+);
+
+  logic [DATA_WIDTH-1:0] s_shift_d, s_shift_q;
+
+  assign dat_o                     = s_shift_q;
+  for(genvar i = 0; i <= DATA_WIDTH - 1; i++) begin
+    if(i == DATA_WIDTH - 1) begin
+      assign s_shift_d[i] = wr_i ? dat_i[i] : s_shift_q[0] ^ s_shift_q[1];
+    end else begin
+      assign s_shift_d[i] = wr_i ? dat_i[i] : s_shift_q[i+1];
+    end
+  end
+
+  dffrc #(DATA_WIDTH, SEED) u_shift_dffrc (
+      clk_i,
+      rst_n_i,
+      s_shift_d,
+      s_shift_q
+  );
+
 endmodule
