78c75c76f06f4ed1259661ecee45b74917de0b95
maksyuki <maksyuki@126.com>
2023-11-15 10:47:21 +0800

    feat: add print and cmp check tasks

--- rtl/verif/helper.sv
+++ rtl/verif/helper.sv
@@ -2,13 +2,30 @@
 `define INC_HELPER_SV
 
 class Helper;
-  stirng name;
+  typedef enum {
+    NORM  = 0,
+    DEBUG,
+    INFO,
+    WARN,
+    ERR
+  } log_lev_t;
 
-  function new(string name = "sv_helper");
-    this.name = name;
-  endfunction
+  typedef enum {
+    EQUAL,
+    GRET,
+    LESS,
+    GREQ,
+    LEEQ
+  } cmp_t;
+
+  static string    name       = "sv_helper";
+  static log_lev_t log_lev    = INFO;
+  static int       all_num    = 0;
+  static int       failed_num = 0;
 
   static function void start_banner();
+    $display("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    $display("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
     $display("  ______    ______    ______    ______         ______  _______  ");
     $display(" /      \\  /      \\  /      \\  /      \\       |      \\|       \\ ");
     $display("|  $$$$$$\\|  $$$$$$\\|  $$$$$$\\|  $$$$$$\\       \\$$$$$$| $$$$$$$\\");
@@ -23,10 +40,33 @@ class Helper;
   endfunction
 
   static function void end_banner();
+    test_summary();
     $display("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
     $display("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
   endfunction
 
+  static function void test_summary();
+    $display("%t === [ALL TEST]: %0d [FAILED TEST]: %0d [PASS TEST]: %0d === ", $time, all_num,
+             failed_num, all_num - failed_num);
+  endfunction
+
+  static task print(input string info);
+    if (log_lev > NORM) begin
+      $display("%t [%s] %s", $time, log_lev, info);
+    end
+  endtask
+
+  static task check(input string name, input bit [31:0] actual, expected);
+    if (log_lev > NORM) begin
+      $display("%t [%s] checking %s actual: %h, expected: %h", $time, log_lev, name, actual,
+               expected);
+    end
+
+    all_num++;
+    if (actual !== expected) begin
+      failed_num++;
+    end
+  endtask
 endclass
 
 `endif
