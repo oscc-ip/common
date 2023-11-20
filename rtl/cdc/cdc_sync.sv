38c977036873e315e41d891f77a1ff17fae99314
maksyuki <maksyuki@126.com>
2023-11-20 19:39:28 +0800

    feat: add cdc sync det module

--- rtl/cdc/cdc_sync.sv
+++ rtl/cdc/cdc_sync.sv
@@ -42,6 +42,40 @@ module cdc_sync #(
     end
   end
 
-  assign dat_o = s_sync_dat[STAGE-1];
+  assign dat_o     = s_sync_dat[STAGE-1];
+endmodule
+
+module cdc_sync_det #(
+    parameter int STAGE      = 2,
+    parameter int DATA_WIDTH = 1
+) (
+    input  logic                  clk_i,
+    input  logic                  rst_n_i,
+    input  logic [DATA_WIDTH-1:0] dat_i,
+    output logic [DATA_WIDTH-1:0] dat_pre_o,
+    output logic [DATA_WIDTH-1:0] dat_o
+);
+
+  logic [DATA_WIDTH-1:0] s_sync_dat[0:STAGE-1];
+  for (genvar i = 0; i < STAGE; i++) begin
+    if (i == 0) begin
+      dffr #(DATA_WIDTH) u_sync_dffr (
+          clk_i,
+          rst_n_i,
+          dat_i,
+          s_sync_dat[0]
+      );
+    end else begin
+      dffr #(DATA_WIDTH) u_sync_dffr (
+          clk_i,
+          rst_n_i,
+          s_sync_dat[i-1],
+          s_sync_dat[i]
+      );
+    end
+  end
+
+  assign dat_pre_o = s_sync_dat[STAGE-2];
+  assign dat_o     = s_sync_dat[STAGE-1];
 endmodule
 `endif
