ebf00076fbd315c72950153432827f0fe0895392
maksyuki <maksyuki@126.com>
2023-12-22 20:12:26 +0800

    feat: add pll module

---
+++ rtl/tech/pll.sv
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// common is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+// this file only include behavioral model, for ASIC tape-out need to reimplement those models
+module pll (
+    
+);
+
+`ifdef BACKEND
+  $error("need to instantiate specific technology cell in this block and remove this statement");
+`else
+  $display("hello");
+`endif
+endmodule
