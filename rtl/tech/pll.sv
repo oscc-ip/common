32fffb4f8f7e70f98d1ab47bcebe4c487c9adbec
maksyuki <maksyuki@126.com>
2023-12-26 19:24:50 +0800

    feat: add pll sim model impl

--- rtl/tech/pll.sv
+++ rtl/tech/pll.sv
@@ -9,13 +9,27 @@
 // See the Mulan PSL v2 for more details.
 
 // this file only include behavioral model, for ASIC tape-out need to reimplement those models
-module pll (
-    
+
+`ifndef INC_PLL_SV
+`define INC_PLL_SV
+
+module tech_pll #(
+) (
+    input  logic        fref_i,
+    input  logic [ 5:0] refdiv_i,
+    input        [11:0] fbdiv_i,
+    input  logic [ 2:0] postdiv1_i,
+    input  logic [ 2:0] postdiv2_i,
+    output logic        pll_lock_o,
+    output logic        pll_clk_o
 );
 
 `ifdef BACKEND
   $error("need to instantiate specific technology cell in this block and remove this statement");
 `else
-  $display("hello");
+  assign pll_lock_o = 1'b1;
+  assign pll_clk_o  = fref_i;
 `endif
 endmodule
+
+`endif
