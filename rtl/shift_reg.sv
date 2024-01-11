079468e54f571e8d15d5a9c5e4da8cbddc396c4e
maksyuki <maksyuki@126.com>
2024-01-11 20:17:05 +0800

    feat: add input type support

--- rtl/shift_reg.sv
+++ rtl/shift_reg.sv
@@ -17,6 +17,8 @@
 `define SHIFT_REG_TYPE_ARICH 2'b00
 `define SHIFT_REG_TYPE_LOGIC 2'b01
 `define SHIFT_REG_TYPE_LOOP  2'b10
+`define SHIFT_REG_TYPE_SERI  2'b11
+
 `define SHIFT_REG_DIR_KEEP   2'b00
 `define SHIFT_REG_DIR_LEFT   2'b01
 `define SHIFT_REG_DIR_RIGHT  2'b10
@@ -30,8 +32,9 @@ module shift_reg #(
     input  logic                  rst_n_i,
     input  logic [           1:0] type_i,
     input  logic [           1:0] dir_i,
-    input  logic                  load_i,
+    input  logic [           1:0] ld_en_i,
     input  logic                  sft_en_i,
+    input  logic                  ser_dat_i,
     input  logic [DATA_WIDTH-1:0] par_data_i,
     output logci                  ser_dat_o,
     output logic [DATA_WIDTH-1:0] par_data_o
@@ -40,21 +43,26 @@ module shift_reg #(
   logic [DATA_WIDTH-1:0] s_sf_d, s_sf_q;
   logic s_sf_en;
 
-
-  assign ser_dat_o = dir_i == `SHIFT_REG_DIR_RIGHT ? s_sf_q[DATA_WIDTH-1] : s_sf_q[0];
+  assign ser_dat_o = dir_i == `SHIFT_REG_DIR_RIGHT ? s_sf_q[0] : s_sf_q[DATA_WIDTH-1];
   assign par_data_o = s_sf_q;
 
-  assign s_sf_en = dir_i != '0 && sft_en_i;
+  assign s_sf_en = ld_en_i || (dir_i != `SHIFT_REG_DIR_KEEP && sft_en_i);
   always_comb begin
-    s_sf_d = s_sf_q;
-    if (load_i) begin
+    if (ld_en_i) begin
       s_sf_d = par_data_i;
     end else begin
+      s_sf_d = s_sf_q;
       unique case (dir_i)
         `SHIFT_REG_DIR_KEEP: s_sf_d = s_sf_q;
         `SHIFT_REG_DIR_LEFT: begin
           s_sf_d[DATA_WIDTH-1:1] = s_sf_q[DATA_WIDTH-2:0];
-          s_sf_d[0] = type_i == SHIFT_REG_TYPE_LOOP ? s_sf_q[DATA_WIDTH-1] : 1'b0;
+          unique case (type_i)
+            `SHIFT_REG_TYPE_ARICH: s_sf_d[0] = 1'b0;
+            `SHIFT_REG_TYPE_LOGIC: s_sf_d[0] = 1'b0;
+            `SHIFT_REG_TYPE_LOOP: s_sf_d[0] = s_sf_q[DATA_WIDTH-1];
+            `SHIFT_REG_TYPE_SERI: s_sf_d[0] = ser_dat_i;
+            default: s_sf_d[0] = 1'b0;
+          endcase
         end
         `SHIFT_REG_DIR_RIGHT: begin
           s_sf_d[DATA_WIDTH-2:0] = s_sf_q[DATA_WIDTH-1:1];
@@ -62,6 +70,7 @@ module shift_reg #(
             `SHIFT_REG_TYPE_ARICH: s_sf_d[DATA_WIDTH-1] = s_sf_q[DATA_WIDTH-1] == 1'b1;
             `SHIFT_REG_TYPE_LOGIC: s_sf_d[DATA_WIDTH-1] = 1'b0;
             `SHIFT_REG_TYPE_LOOP: s_sf_d[DATA_WIDTH-1] = s_sf_q[0];
+            `SHIFT_REG_TYPE_SERI: s_sf_d[DATA_WIDTH-1] = ser_dat_i;
             default: s_sf_d[DATA_WIDTH-1] = 1'b0;
           endcase
         end
