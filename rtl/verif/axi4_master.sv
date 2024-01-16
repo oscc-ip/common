5543efe6e3de9d4c6cca3112fb0ff75d3998f0b2
maksyuki <maksyuki@126.com>
2024-01-16 15:01:01 +0800

    feat: add rd oper task and id check logic

--- rtl/verif/axi4_master.sv
+++ rtl/verif/axi4_master.sv
@@ -17,8 +17,8 @@
 
 class AXI4Master extends TestBase;
   string                                        name;
-  logic                  [`AXI4_DATA_WIDTH-1:0] rd_data;
-  logic                  [`AXI4_DATA_WIDTH-1:0] wr_data;
+  logic                  [`AXI4_DATA_WIDTH-1:0] rd_data[$];
+  logic                  [`AXI4_DATA_WIDTH-1:0] wr_data[$];
   virtual axi4_if.master                        axi4;
 
   extern function new(string name = "axi4_master", virtual axi4_if.master axi4);
@@ -28,7 +28,9 @@ class AXI4Master extends TestBase;
       input bit [2:0] size, input bit [1:0] burst, input bit [`AXI4_DATA_WIDTH-1:0] data[$],
       input bit [`AXI4_DATA_WIDTH/8-1:0] strb);
 
-  // extern task automatic read(input bit [31:0] addr);
+  extern task automatic read(input bit [`AXI4_ID_WIDTH-1:0] id,
+                             input bit [`AXI4_ADDR_WIDTH-1:0] addr, input bit [7:0] len,
+                             input bit [2:0] size, input bit [1:0] burst);
   // extern task automatic wr_rd_check(input bit [31:0] addr, string name, input bit [63:0] data,
   //                                   input Helper::cmp_t cmp_type,
   //                                   input Helper::log_lev_t log_level = Helper::NORM);
@@ -42,8 +44,10 @@ endclass
 
 function AXI4Master::new(string name, virtual axi4_if.master axi4);
   super.new();
-  this.name = name;
-  this.axi4 = axi4;
+  this.name    = name;
+  this.rd_data = {};
+  this.wr_data = {};
+  this.axi4    = axi4;
 endfunction
 
 task automatic AXI4Master::init();
@@ -104,7 +108,7 @@ task automatic AXI4Master::write(
     @(posedge this.axi4.aclk);
   end
   #1;
-  $display("%t aw trigger", $time);
+  // $display("%t aw trigger", $time);
 
   this.axi4.awid    = '0;
   this.axi4.awaddr  = 'x;
@@ -124,41 +128,69 @@ task automatic AXI4Master::write(
       @(posedge this.axi4.aclk);
     end
     #1;
-    $display("%t w burst trigger", $time);
+    // $display("%t w burst trigger", $time);
   end
 
   this.axi4.wdata  = 'x;
   this.axi4.wstrb  = '0;
   this.axi4.wlast  = '0;
   this.axi4.wvalid = '0;
+
+  this.axi4.bready = 1'b1;
+  @(posedge this.axi4.aclk);
+  while (~this.axi4.bvalid) begin
+    @(posedge this.axi4.aclk);
+  end
+
+  if (this.axi4.bid != id) begin
+    $error("%t [wr mismatch id] awid is %d, bid: %d", $time, id, this.axi4.bid);
+  end
+  #1;
+  this.axi4.bready = 1'b0;
 endtask
 
-// task automatic AXI4Master::read(input bit [31:0] addr);
-// logic [31:0] val;
-// this.axi4.pprot = '0;
-// this.axi4.pstrb = '0;
-// @(posedge this.axi4.pclk);
-// #1;
-// this.axi4.paddr  = addr;
-// this.axi4.psel   = 1'b1;
-// this.axi4.pwrite = 1'b0;
-// this.axi4.pwdata = 'x;
+task automatic AXI4Master::read(input bit [`AXI4_ID_WIDTH-1:0] id,
+                                input bit [`AXI4_ADDR_WIDTH-1:0] addr, input bit [7:0] len,
+                                input bit [2:0] size, input bit [1:0] burst);
 
-// @(posedge this.axi4.pclk);
-// #1;
-// this.axi4.penable = 1'b1;
-// @(posedge this.axi4.pclk && this.axi4.pready);
-// val          = this.axi4.prdata;
-// this.rd_data = val;
-// #1;
-// this.axi4.paddr   = 'x;
-// this.axi4.psel    = '0;
-// this.axi4.penable = '0;
-// this.axi4.pwrite  = '0;
-// this.axi4.pwdata  = 'x;
+  this.rd_data = {};
+  // ar channel
+  @(posedge this.axi4.aclk);
+  #1;
+  this.axi4.arid    = id;
+  this.axi4.araddr  = addr;
+  this.axi4.arlen   = len + 1'b1;
+  this.axi4.arsize  = size;
+  this.axi4.arburst = burst;
+  this.axi4.arvalid = 1'b1;
 
+  @(posedge this.axi4.aclk);
+  while (~this.axi4.arready) begin
+    @(posedge this.axi4.aclk);
+  end
+  #1;
 
-// endtask
+  this.axi4.arid    = '0;
+  this.axi4.araddr  = 'x;
+  this.axi4.arlen   = '0;
+  this.axi4.arsize  = `AXI4_BURST_SIZE_1BYTE;
+  this.axi4.arburst = `AXI4_BURST_TYPE_FIXED;
+  this.axi4.arvalid = '0;
+
+  // r burst channel
+  this.axi4.rready  = 1'b1;
+  for (int i = 0; i < len + 1'b1; i++) begin
+    @(posedge this.axi4.aclk);
+    while (~this.axi4.rvalid) begin
+      @(posedge this.axi4.aclk);
+    end
+    this.rd_data.push_back(this.axi4.rdata);
+    $display("%t: this.axi4.rdata: %h", $time, this.axi4.rdata);
+    #1;
+  end
+
+  this.axi4.rready = 1'b0;
+endtask
 
 // task automatic AXI4Master::wr_rd_check(input bit [31:0] addr, string name, input bit [63:0] data,
 //                                        input Helper::cmp_t cmp_type,
